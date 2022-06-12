9250c19a06fb088e25b9d11c922e2aed62e83f66
Dan Boitnott <dboitnot@gmail.com>
2022-06-12 09:03:11 -0500

    Fixed escaping issue

--- build/feature_awsmfa.sh
+++ build/feature_awsmfa.sh
@@ -1,4 +1,4 @@
 curl https://raw.githubusercontent.com/sigdba/sig-shared-sceptre/main/templates/IamMfa/awsmfa.sh -o /opt/awsmfa.sh
 chmod 755 /opt/awsmfa.sh
 
-echo "alias awsmfa='eval $(/opt/awsmfa.sh)'" >/etc/profile.d/awsmfa.sh
+echo "alias awsmfa='eval \$(/opt/awsmfa.sh)'" >/etc/profile.d/awsmfa.sh
